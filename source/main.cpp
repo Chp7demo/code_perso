31c5eca830ee5aadf2739737d351a8948b57a973
rem7 <chp7demo@gmail.com>
2024-10-06 12:15:11 +0200

    matrice depuis fichier

--- source/main.cpp
+++ source/main.cpp
@@ -1,1 +1,56 @@
-//main
+#include <iostream>
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#include <fstream>
+
+
+using namespace std;
+
+/**
+ * The machines are gaining ground. Time to show them what we're really made of...
+ **/
+typedef vector<string> gridMatr; 
+
+
+
+int main(int argc, char * argv[])
+{
+    int width; // the number of cells on the X axis
+    //cin >> width; cin.ignore();
+    width=10;// a changer selon le jeu simulé 
+
+    int height; // the number of cells on the Y axis
+    //cin >> height; cin.ignore();
+    height=10; // a changer selon jeu simulé 
+
+    /*for (int i = 0; i < height; i++) {
+        string line;
+        getline(cin, line); // width characters, each either a number or a '.'
+    }*/
+
+
+    ifstream inputFile;
+    inputFile.open(argv[1]);
+    
+    gridMatr gm;
+    
+    string lignefich;
+    while(getline(inputFile,lignefich)){gm.push_back(lignefich);} 
+
+    //verif matrice
+    for(auto el: gm) cout <<el<< endl;
+
+
+
+    // Write an action using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+
+    // Two coordinates and one integer: a node, one of its neighbors, the number of links connecting them.
+    cout << "rien pour l'instant" << endl;
+
+    return 0;
+}
