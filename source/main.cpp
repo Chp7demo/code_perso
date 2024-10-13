5758df94bcb6e9284da30e77a9a947abad15e8af
rem7 <chp7demo@gmail.com>
2024-10-13 21:02:59 +0200

    petite avancée

--- source/main.cpp
+++ source/main.cpp
@@ -5,6 +5,11 @@
 #include <algorithm>
 
 #include <fstream>
+#include <map>
+#include <string>
+
+#include <stdio.h>
+#include <stdlib.h>
 
 
 using namespace std;
@@ -14,6 +19,61 @@ using namespace std;
  **/
 typedef vector<string> gridMatr; 
 
+class Node
+{
+    int power;
+    int nb_bridge;
+    int v_up, v_down, v_left, v_right; // index des voisins -1 si pas de voisins
+
+
+    public:
+    void set_power(int _power){power=_power;}
+    int get_power(){return power;}
+    
+    int get_v_up(){return v_up;}
+    int get_v_down(){return v_down;}
+    int get_v_left(){return v_left;}
+    int get_v_right(){return v_right;}
+    
+    
+    int set_v_up(int _v_up){v_up=_v_up;}
+    int set_v_down(int _v_down){v_down=_v_down;}
+    int set_v_left(int _v_left){v_left=_v_left;}
+    int set_v_right(int _v_rigth){v_right=v_right;}
+    
+};
+
+class grid
+{
+vector<Node> all_nodes; 
+map<int,int> bridge_map;
+
+public:
+
+grid(gridMatr gm);
+
+};
+
+
+grid::grid(gridMatr gm)
+{
+ // liste des nodes   
+    for(auto ligne:gm)
+    {
+        for(auto col:ligne){
+            if(col!='.'){
+                Node new_node;
+                string col_s;
+                col_s+=col;
+                int power=std::stoi(col_s);
+                new_node.set_power(power);
+                all_nodes.push_back(new_node);
+                }
+            }
+    }
+
+ // parcourir la liste des node pour attribuer les voisins   
+}
 
 
 int main(int argc, char * argv[])
